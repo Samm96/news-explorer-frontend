42311440aac4b5edef459ac6a9f285a1228c8f41
Samm96 <sihorsch96@gmail.com>
2022-08-06 17:54:15 -0400

    Fix: fonts / Add: BrowserRouter to indexjs / Add: logo css

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,19 @@
-import './Logo.css';
-import { Link } from 'react-router-dom';
+import "./Logo.css";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo/NewsExplorer-logo_svg.svg";
 
 /** Logo Component
- * 
+ *
  * The logo, when clicked, will return user to home page.
- * 
+ *
  */
 
 const Logo = () => {
-    return (
-
-    )
-}
+  return (
+    <Link to="/">
+      <p className="logo">NewsExplorer</p>
+    </Link>
+  );
+};
 
 export default Logo;
