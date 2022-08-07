1a45ff66d364cb98525bfd3eb8a362136bf94223
Samm96 <sihorsch96@gmail.com>
2022-08-07 12:09:17 -0400

    Adjust: Logo comp margin / Add: Header component, Headercss

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
+import Logo from '../Logo/Logo';
+
+const Header = ({isLoggedIn, userName}) => {
+
+    return (
+        <div className="header">
+            <div className="header__container">
+                <Logo />
+                <Navigation />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
