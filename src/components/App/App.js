ac939cc616e6722bada9af795f5f2283f6c88112
Samm96 <sihorsch96@gmail.com>
2022-08-06 22:46:48 -0400

    Fix: issue of not toggling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navigation from '../Navigation/Navigation';
 
 const App = () => {
+  // placeholder
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="page">
       <div className="page-wrapper">
-        <Navigation />
+        <Navigation isLoggedIn={isLoggedIn}/>
       </div>
     </div>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,32 +1,18 @@
+import React from 'react';
 import "./Navigation.css";
 import { NavLink } from "react-router-dom";
-import blackIcon from "../../images/Icons/button-icon.svg";
+// import blackIcon from "../../images/Icons/button-icon.svg";
 import whiteIcon from "../../images/Icons/button-icon-white.svg";
 
-const Navigation = (isLoggedIn, userName) => {
+const Navigation = ({isLoggedIn, userName}) => {
   const placeholderUser = "Samantha";
 
   return (
     <nav className="navigation">
       {isLoggedIn ? (
-        <ul className="navigation__container">
-          <li className="navigation__link">
-            <NavLink
-              to="/"
-              className="navigation__home navigation__home_type_selected"
-              aria-label="nav link"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <button className="navigation__button">Sign in</button>
-          </li>
-        </ul>
-      ) : (
         <ul className="navigation__container navigation__container_type_signedin">
           <li className="navigation__link">
-            <NavLink to="/" className="navigation__home" aria-label="nav link">
+            <NavLink to="/" className="navigation__home navigation__home_type_selected" aria-label="nav link">
               Home
             </NavLink>
           </li>
@@ -41,14 +27,28 @@ const Navigation = (isLoggedIn, userName) => {
           </li>
           <li>
             <button className="navigation__button">
-              {/* {userName} */}
-              {placeholderUser}
+              {placeholderUser || userName}
               <img
                 className="navigation__button-img"
                 src={whiteIcon}
                 alt="signout"
               />
             </button>
+          </li>
+        </ul>
+      ) : (
+        <ul className="navigation__container">
+          <li className="navigation__link">
+            <NavLink
+              to="/"
+              className="navigation__home navigation__home_type_selected"
+              aria-label="nav link"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <button className="navigation__button">Sign in</button>
           </li>
         </ul>
       )}
