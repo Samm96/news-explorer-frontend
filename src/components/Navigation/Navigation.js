eab084cb1f3d7ce79bea1e8df149f7196c64c774
Samm96 <sihorsch96@gmail.com>
2022-08-06 22:11:41 -0400

    Add: css for loggedin / Fix: adjusted css

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,25 +3,44 @@ import { NavLink } from "react-router-dom";
 import blackIcon from "../../images/Icons/button-icon.svg";
 import whiteIcon from "../../images/Icons/button-icon-white.svg";
 
-
 const Navigation = (isLoggedIn, userName) => {
   return (
     <nav className="navigation">
       <ul className="navigation__container">
-        {/* <li className="navigation__link">
+         <li className="navigation__link">
           <NavLink to="/" className="navigation__home" aria-label="nav link">Home</NavLink>
         </li>
         <li>
           <button className="navigation__button">Sign in</button>
-        </li> */}
+        </li> 
+        </ul>
+
+
+      <ul className="navigation__container navigation__container_type_signedin">
         <li className="navigation__link">
-          <NavLink to="/" className="navigation__home" aria-label="nav link">Home</NavLink>
+          <NavLink to="/" className="navigation__home" aria-label="nav link">
+            Home
+          </NavLink>
         </li>
         <li className="navigation__link">
-            <NavLink to="/saved-news" className="navigation__saved-articles" aria-label="nav link">Saved articles</NavLink>
+          <NavLink
+            to="/saved-news"
+            className="navigation__saved-articles"
+            aria-label="nav link"
+          >
+            Saved articles
+          </NavLink>
         </li>
         <li>
-          <button className="navigation__button">User's Name<img className="navigation__button-img" src={whiteIcon} alt='signout'/></button>
+          <button className="navigation__button">
+          {/* {userName} */}
+            Samantha
+            <img
+              className="navigation__button-img"
+              src={whiteIcon}
+              alt="signout"
+            />
+          </button>
         </li>
       </ul>
     </nav>
