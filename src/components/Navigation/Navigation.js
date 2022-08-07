7b299212b3aa1801a8286d200e2df694c276c034
Samm96 <sihorsch96@gmail.com>
2022-08-06 22:14:12 -0400

    Add: implemented isLoggedIn

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,45 +4,50 @@ import blackIcon from "../../images/Icons/button-icon.svg";
 import whiteIcon from "../../images/Icons/button-icon-white.svg";
 
 const Navigation = (isLoggedIn, userName) => {
+  const placeholderUser = "Samantha";
+
   return (
     <nav className="navigation">
-      <ul className="navigation__container">
-         <li className="navigation__link">
-          <NavLink to="/" className="navigation__home" aria-label="nav link">Home</NavLink>
-        </li>
-        <li>
-          <button className="navigation__button">Sign in</button>
-        </li> 
+      {isLoggedIn ? (
+        <ul className="navigation__container">
+          <li className="navigation__link">
+            <NavLink to="/" className="navigation__home" aria-label="nav link">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <button className="navigation__button">Sign in</button>
+          </li>
         </ul>
-
-
-      <ul className="navigation__container navigation__container_type_signedin">
-        <li className="navigation__link">
-          <NavLink to="/" className="navigation__home" aria-label="nav link">
-            Home
-          </NavLink>
-        </li>
-        <li className="navigation__link">
-          <NavLink
-            to="/saved-news"
-            className="navigation__saved-articles"
-            aria-label="nav link"
-          >
-            Saved articles
-          </NavLink>
-        </li>
-        <li>
-          <button className="navigation__button">
-          {/* {userName} */}
-            Samantha
-            <img
-              className="navigation__button-img"
-              src={whiteIcon}
-              alt="signout"
-            />
-          </button>
-        </li>
-      </ul>
+      ) : (
+        <ul className="navigation__container navigation__container_type_signedin">
+          <li className="navigation__link">
+            <NavLink to="/" className="navigation__home" aria-label="nav link">
+              Home
+            </NavLink>
+          </li>
+          <li className="navigation__link">
+            <NavLink
+              to="/saved-news"
+              className="navigation__saved-articles"
+              aria-label="nav link"
+            >
+              Saved articles
+            </NavLink>
+          </li>
+          <li>
+            <button className="navigation__button">
+              {/* {userName} */}
+              {placeholderUser}
+              <img
+                className="navigation__button-img"
+                src={whiteIcon}
+                alt="signout"
+              />
+            </button>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
