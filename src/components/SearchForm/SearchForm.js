445dd192170903a8a683888501b2abd9bc4153c5
Samm96 <sihorsch96@gmail.com>
2022-08-07 22:24:24 -0400

    Add: SearchForm jsx & css / Adjust: Header css padding / Delete: page wrapper

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,10 +1,33 @@
 import "./SearchForm.css";
 
-const SearchForm = () => {
+const SearchForm = ({ handleSubmit, children }) => {
   return (
-  <div>
-    
-  </div>
+    <div className="search-form">
+      {children}
+      <div className="search-form__container">
+        <p className="search-form__text">What's going on in the world?</p>
+        <p className="search-form__subtext">
+          Find the latest news on any topic and save them in your personal
+          account.
+        </p>
+        <form name="search" onSubmit={handleSubmit}>
+          <div className="search-form__bar">
+            <input
+              className="search-form__input"
+              type="text"
+              placeholder="Enter topic"
+            ></input>
+            <button
+              className="search-form__button"
+              type="search"
+              id="search-button"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
