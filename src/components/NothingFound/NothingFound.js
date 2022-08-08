06979a12020aea77e7bbb833d3c7544101de1d62
Samm96 <sihorsch96@gmail.com>
2022-08-07 23:10:38 -0400

    Add: NothingFound component base JSX & icon

---
+++ src/components/NothingFound/NothingFound.js
@@ -0,0 +1,12 @@
+import './NothingFound.css';
+import notFoundImg from '../../images/not-found_icon.png';
+
+const NothingFound = () => {
+    return (
+        <div className="not-found">
+            
+        </div>
+    )
+}
+
+export default NothingFound;
