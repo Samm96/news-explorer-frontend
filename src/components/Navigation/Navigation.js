448ffef16f4577b0eeea5b8443cfd3ae9b6af3c0
Samm96 <sihorsch96@gmail.com>
2022-08-09 01:15:20 -0400

    Add: division in text color in Navigation js, props in Header and Logo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navigation.css";
 import { NavLink } from "react-router-dom";
 // import blackIcon from "../../images/Icons/button-icon.svg";
 import whiteIcon from "../../images/Icons/button-icon-white.svg";
 
-const Navigation = ({isLoggedIn, userName}) => {
+const Navigation = ({isLoggedIn, userName, savedPage, homePage}) => {
   const placeholderUser = "Samantha";
 
+  const whiteText = {
+    signInButton: "navigation__button",
+    homeButton: "navigation__home",
+    savedArticles: "navigation__saved-articles",
+  }
+
+  const blackText = {
+    signInButton: `${whiteText.signInButton} navigation__button_type_page`,
+    homeButton: `${whiteText.homeButton} navigation__home_type_page`,
+    savedArticles: `${whiteText.savedArticles} navigation__saved-articles_type_page`,
+  }
+
+  const whiteTextSelected = {
+    savedArticles: `${whiteText.savedArticles} navigation__saved-articles_type_selected`,
+    homeButton: `${whiteText.homeButton} navigation__home navigation__home_type_selected`,
+  }
+
+  const blackTextSelected = {
+    savedArticles: `${blackText.savedArticles} navigation__saved-articles_type_selected-page`,
+    homeButton: `${blackText.homeButton} navigation__home_type_selected-page`,
+  }
+
+
   return (
-    <nav className="navigation">
+    <nav className="navigation"> 
       {isLoggedIn ? (
         <ul className="navigation__container navigation__container_type_signedin">
           <li className="navigation__link">
