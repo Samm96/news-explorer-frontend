8de503cea060306570ce2d90adff381b33f438df
Samm96 <sihorsch96@gmail.com>
2022-08-10 16:45:41 -0400

    Add: cards css in SavedNews

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -1,31 +1,42 @@
-import './SavedNews.css';
-import NewsCardList from '../NewsCardList/NewsCardList';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import "./SavedNews.css";
+import NewsCardList from "../NewsCardList/NewsCardList";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import NewsCard from "../NewsCard/NewsCard";
 
-const SavedNews = ({userName}) => {
-    const placeholder = "Samantha"
+const SavedNews = ({ userName }) => {
+  const placeholder = "Samantha";
 
-    /** keywords is probably going to be an array */
-    const placeholderData = {
-        savedArticles: 5,
-        keywords: "Nature, Yellowstone and 2 others"
-    }
+  /** keywords is probably going to be an array */
+  const placeholderData = {
+    savedArticles: 5,
+    keywords: "Nature, Yellowstone and 2 others",
+  };
 
-     return (
-        <div className="saved-news">
-            <Header isLoggedIn={true} logoColor={"black"} textColor={"black"} />
-            <div className="saved-news__text-container">
-                <p className="saved-news__title">Saved articles</p>
-                <p className="saved-news__saved">{placeholder || userName}, you have {placeholderData.savedArticles} saved articles</p>
-                <p className="saved-news__keywords">By keywords: <b>{placeholderData.keywords}</b></p>
-            </div>
-            <div className="saved-news__news-container">
-                <NewsCardList />
-            </div>
-            <Footer />
+  return (
+    <div className="saved-news">
+      <Header isLoggedIn={true} logoColor={"black"} textColor={"black"} />
+      <div className="saved-news__text-container">
+        <p className="saved-news__title">Saved articles</p>
+        <p className="saved-news__saved">
+          {placeholder || userName}, you have {placeholderData.savedArticles}{" "}
+          saved articles
+        </p>
+        <p className="saved-news__keywords">
+          By keywords: <b>{placeholderData.keywords}</b>
+        </p>
+      </div>
+      <div className="saved-news__news-container">
+        <div className="saved-news__cards">
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
+          <NewsCard />
         </div>
-    )
-}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default SavedNews;
