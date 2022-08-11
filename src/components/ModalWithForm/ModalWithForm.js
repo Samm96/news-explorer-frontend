47142f96b1e54a43479e05bbf88be01f5149f119
Samm96 <sihorsch96@gmail.com>
2022-08-10 21:02:40 -0400

    Add: ModalWithForm jsx

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -4,7 +4,7 @@ const ModalWithForm = () => {
     return (
         <div className="modal">
             <div className="modal__container">
-                <p className="modal__title">{modalTitle}</p>
+                {/* <p className="modal__title">{modalTitle}</p> */}
             </div>
         </div>
     )
