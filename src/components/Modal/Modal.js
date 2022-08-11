48873f90b8740bf99fdda7dca5ff7d017ea64b86
Samm96 <sihorsch96@gmail.com>
2022-08-10 22:47:10 -0400

    Add: commended out more jsx

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,28 +1,26 @@
 import "./Modal.css";
-import { NavLink } from "react-router-dom";
 
 const Modal = ({
   isOpen,
   modalTitle,
   isButton,
   buttonText,
-  linkText,
-  openModal,
+  // linkText,
+  // openModal,
   children,
 }) => {
   return (
-    // <div className={`modal ${isOpen && "modal_open"}`}>
-    <div className="modal modal_open">
+    <div className={`modal ${isOpen && "modal_open"}`}>
       <div className="modal__container">
         <button className="close-button"></button>
         <p className="modal__title">{"title" || modalTitle}</p>
         {children}
-        {/* {isButton ? ( */}
+        {isButton ? (
         <button className="modal__button">{"button" || buttonText}</button>
-        {/* ) : (
+        ) : (
           ""
-        )} */}
-        
+        )}
+
         {/* <p className="modal__text">
           or{" "}
           <button className="modal__link" onClick={openModal}>
