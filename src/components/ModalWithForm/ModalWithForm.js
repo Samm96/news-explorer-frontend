99181e787b634572961ca94b684b675da4e28889
Samm96 <sihorsch96@gmail.com>
2022-08-10 20:40:31 -0400

    Add: some modal with form jsx

---
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -0,0 +1,13 @@
+import './ModalWithForm.css';
+
+const ModalWithForm = () => {
+    return (
+        <div className="modal">
+            <div className="modal__container">
+                <p className="modal__title">{modalTitle}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ModalWithForm;
