d68fe32e5b5fadb481cdb6bbd26b7d3c9fc7756c
Samm96 <sihorsch96@gmail.com>
2022-08-12 23:43:23 -0400

    Add: more jsx to NavModal

--- src/components/NavigationModal/NavigationModal.js
+++ src/components/NavigationModal/NavigationModal.js
@@ -1,15 +1,22 @@
-import './NavigationModal.css';
-import Logo from '../Logo/Logo';
+import "./NavigationModal.css";
+import Logo from "../Logo/Logo";
+import { NavLink } from "react-router-dom";
 
-const NavigationModal = () => {
-    return (
-        <div className="nav-modal">
-            <div className="nav-modal__container">
-                <Logo logoColor={"white"}/>
-                <button className="nav-modal__close-button"></button>
-            </div>
-        </div>
-    )
-}
+const NavigationModal = ({isOpen, isLoggedIn}) => {
+  return (
+    <div className={`nav-modal ${isOpen && "nav-modal_open"}`}>
+      <div className="nav-modal__container">
+        <Logo logoColor={"white"} />
+        <button className="nav-modal__close-button"></button>
+      </div>
+      <div className="nav-modal__link-container">
+        <NavLink to="/" className="nav-modal__link">
+          Home
+        </NavLink>
+        <button className="nav-modal__button">{isLoggedIn ? "Sign up" : "Sign in"}</button>
+      </div>
+    </div>
+  );
+};
 
 export default NavigationModal;
