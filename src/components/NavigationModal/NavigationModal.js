3ea84bfed4fd35464017ddb671424224b14de4ba
Samm96 <sihorsch96@gmail.com>
2022-08-13 00:21:38 -0400

    Add: close navigation modal behavior

--- src/components/NavigationModal/NavigationModal.js
+++ src/components/NavigationModal/NavigationModal.js
@@ -2,13 +2,13 @@ import "./NavigationModal.css";
 import Logo from "../Logo/Logo";
 import { NavLink } from "react-router-dom";
 
-const NavigationModal = ({ isOpen, isLoggedIn }) => {
+const NavigationModal = ({ isOpen, isLoggedIn, onClose }) => {
   return (
     <div className={`nav-modal nav-modal__overlay ${isOpen && "nav-modal_open"}`}>
       <div className="nav-modal__container">
         <div className="nav-modal__header-container">
           <Logo logoColor={"white"} />
-          <button className="nav-modal__close-button"></button>
+          <button className="nav-modal__close-button" onClick={onClose}></button>
         </div>
         <div className="nav-modal__link-container">
           <NavLink to="/" className="nav-modal__link">
