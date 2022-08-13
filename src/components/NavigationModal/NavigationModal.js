14c868186700ecb5a846c0bc6c7e32fd3740081d
Samm96 <sihorsch96@gmail.com>
2022-08-13 00:19:55 -0400

    Add: overlay for navigation modal

--- src/components/NavigationModal/NavigationModal.js
+++ src/components/NavigationModal/NavigationModal.js
@@ -2,18 +2,22 @@ import "./NavigationModal.css";
 import Logo from "../Logo/Logo";
 import { NavLink } from "react-router-dom";
 
-const NavigationModal = ({isOpen, isLoggedIn}) => {
+const NavigationModal = ({ isOpen, isLoggedIn }) => {
   return (
-    <div className={`nav-modal ${isOpen && "nav-modal_open"}`}>
+    <div className={`nav-modal nav-modal__overlay ${isOpen && "nav-modal_open"}`}>
       <div className="nav-modal__container">
-        <Logo logoColor={"white"} />
-        <button className="nav-modal__close-button"></button>
-      </div>
-      <div className="nav-modal__link-container">
-        <NavLink to="/" className="nav-modal__link">
-          Home
-        </NavLink>
-        <button className="nav-modal__button">{isLoggedIn ? "Sign up" : "Sign in"}</button>
+        <div className="nav-modal__header-container">
+          <Logo logoColor={"white"} />
+          <button className="nav-modal__close-button"></button>
+        </div>
+        <div className="nav-modal__link-container">
+          <NavLink to="/" className="nav-modal__link">
+            Home
+          </NavLink>
+          <button className="nav-modal__button">
+            {isLoggedIn ? "Sign up" : "Sign in"}
+          </button>
+        </div>
       </div>
     </div>
   );
