cfc13ed214e4a82cbd9de39fd96f937d9698213b
Samm96 <sihorsch96@gmail.com>
2022-08-17 18:53:07 -0400

    Add: NewsCardList logic that picks three cards, cards prop to SearchResults, placeholder info in constants

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,12 +1,12 @@
 import "./SearchResults.css";
 import NewsCardList from "../NewsCardList/NewsCardList";
 
-const SearchResults = () => {
+const SearchResults = ({ cards }) => {
   return (
     <div className="results">
       <div className="results__container">
         <p className="results__title">Search Results</p>
-        <NewsCardList buttonType={"save"}/>
+        <NewsCardList cards={cards} buttonType={"save"}/>
         <button className="results__button">Show more</button>
       </div>
     </div>
---
+++ src/utils/constants.js
@@ -0,0 +1,49 @@
+const placeholderCard = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      date: "November 4, 2020",
+      title: "Everyone Needs a Special 'Sit Spot' in Nature",
+      text: 'Ever since I read Richard Louv\'s influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...',
+      source: "TreeHugger",
+      key: "News",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      date: "November 4, 2020",
+      title: "Everyone Needs a Special 'Sit Spot' in Nature",
+      text: 'Ever since I read Richard Louv\'s influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...',
+      source: "TreeHugger",
+      key: "News",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      date: "November 4, 2020",
+      title: "Everyone Needs a Special 'Sit Spot' in Nature",
+      text: 'Ever since I read Richard Louv\'s influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...',
+      source: "TreeHugger",
+      key: "News",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      date: "November 4, 2020",
+      title: "Everyone Needs a Special 'Sit Spot' in Nature",
+      text: 'Ever since I read Richard Louv\'s influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...',
+      source: "TreeHugger",
+      key: "News",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+      date: "November 4, 2020",
+      title: "Everyone Needs a Special 'Sit Spot' in Nature",
+      text: 'Ever since I read Richard Louv\'s influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...',
+      source: "TreeHugger",
+      key: "News",
+    },
+  ];
+
+  export default placeholderCard;
