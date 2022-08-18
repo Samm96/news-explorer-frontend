ab001e40371a3c07fb8ddc659625cd0c503f18f8
Samm96 <sihorsch96@gmail.com>
2022-08-18 17:50:28 -0400

    Fix: logic to find duplicate key words and set what is shown

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -1,46 +1,58 @@
-import React, { useState, useEffect, Children } from 'react';
+import React, { useState, useEffect, Children } from "react";
 import "./SavedNews.css";
 import Footer from "../Footer/Footer";
 import NewsCard from "../NewsCard/NewsCard";
+import { useLinkClickHandler } from "react-router-dom";
 
 const SavedNews = ({ userName, cards }) => {
   const placeholder = "Samantha";
 
   let articlesAmount = cards.length;
   let cardKeywords = [];
-
   cards.forEach((card) => cardKeywords.push(card.key));
-  let keywordString = cardKeywords.toString().split(',').join(', ');
+  let findDuplicates = [...new Set(cardKeywords)];
 
-  console.log(keywordString)
+  let keywords;
 
-  /** keywords is probably going to be an array */
-  const placeholderData = {
-    savedArticles: 5,
-    keywords: "Nature, Yellowstone and 2 others",
-  };
+  switch (findDuplicates.length) {
+    case findDuplicates.length > 2:
+      keywords =
+        findDuplicates.splice(0, 2).toString().split(",").join(", ") +
+        ` and ${cardKeywords.length - 2} others`;
+      break;
+    case (findDuplicates.length = 2):
+      keywords = findDuplicates.toString().split(",").join("and ");
+      break;
+    case (findDuplicates.length = 1):
+      keywords = findDuplicates.toString();
+      break;
+    case (findDuplicates.length = 0):
+      keywords = "You do not have any saved articles";
+      break;
+    default:
+      keywords = "You do not have any saved articles";
+  }
 
   return (
     <div className="saved-news">
       <div className="saved-news__text-container">
         <p className="saved-news__title">Saved articles</p>
         <p className="saved-news__saved">
-          {placeholder || userName}, you have {articlesAmount}{" "}
-          saved articles
+          {placeholder || userName}, you have {articlesAmount} saved articles
         </p>
         <p className="saved-news__keywords">
-          By keywords: <b>{placeholderData.keywords}</b>
+          By keywords: <b>{keywords}</b>
         </p>
       </div>
       <div className="saved-news__news-container">
         <div className="saved-news__cards">
-        {Children.toArray(
-          cards.map((card) => (
-            <>
-              <NewsCard card={card} buttonType={"delete"} />
-            </>
-          ))
-        )}
+          {Children.toArray(
+            cards.map((card) => (
+              <>
+                <NewsCard card={card} buttonType={"delete"} />
+              </>
+            ))
+          )}
         </div>
       </div>
       <Footer />
