977a5ac9058dd6d43ccbee3418ceb5fabca50fec
Samm96 <sihorsch96@gmail.com>
2022-08-19 17:27:26 -0400

    Ran prettier

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -24,27 +24,25 @@ export const login = (email, password) => {
       "Access-Control-Allow-Origin": BASE_URL,
     },
     body: JSON.stringify({ email, password }),
-  })
-  .then((res) => res.json())
+  }).then((res) => res.json());
 };
 
-
 export const checkToken = (token) => {
-    return fetch(`${BASE_URL}/users/me`, {
-        method: 'GET',
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": BASE_URL,
-            Authorization: `Bearer ${token}`,
-        }
-    })
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": BASE_URL,
+      Authorization: `Bearer ${token}`,
+    },
+  })
     .then((res) => {
-        if (res.status === 200) {
-            return res.json();
-        }
+      if (res.status === 200) {
+        return res.json();
+      }
     })
     .catch((res) => {
-        return res;
+      return res;
     });
 };
